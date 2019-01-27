20c79665192e23248cbb0a8303432d87fb998a3f
Mikhail Zheltyshev <test@test.com>
2019-01-27 10:00:43 +0300

    Debugging

--- src/main/java/Fun.java
+++ src/main/java/Fun.java
@@ -1,6 +1,7 @@
 import org.apache.commons.io.FileUtils;
 
 import java.io.*;
+import java.nio.file.Paths;
 
 public class Fun {
     public static void main(String[] args) throws IOException {
@@ -8,5 +9,6 @@ public class Fun {
         System.out.println(destinationDir.getAbsolutePath());
         File textFile = new File(destinationDir,"test.txt");
         FileUtils.writeStringToFile(textFile,"This is a test!!!");
+        System.out.println(Paths.get(".").toAbsolutePath().normalize().toString());
     }
 }
